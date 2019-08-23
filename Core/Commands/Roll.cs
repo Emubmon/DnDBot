50b72dce084896585b9736b2e802352a17e32e83
dylan <patton_ist07@hotmail.com>
2019-08-24 02:04:48 +1000

    Testing commands

--- Core/Commands/Roll.cs
+++ Core/Commands/Roll.cs
@@ -2,10 +2,16 @@
 using System.Collections.Generic;
 using System.Text;
 
+using DnDBot.Core;
+
+using Discord;
+using Discord.Commands;
+
+
 namespace DnDBot.Core.Commands
 {
-    class Roll
+    class Roll : ModuleBase<SocketCommandContext>
     {
-
+        
     }
 }
