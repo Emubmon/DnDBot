018cb4de08bfbade00036f98507b4735d8aaff52
Emubmon <Matthew.edie@gmail.com>
2019-08-23 18:24:32 +1000

    Initial Commit
    Initial commit
    
    Have hello world command only.

---
+++ Core/Commands/HelloWorld.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using Discord;
+using Discord.Commands;
+
+namespace DnD_Bot.Core.Commands
+{
+    public class HelloWorld : ModuleBase<SocketCommandContext>
+    {
+        [Command("hello"), Alias("helloworld, world"), Summary("Hello world command")]
+        public async Task Sjustin()
+        {
+            await Context.Channel.SendMessageAsync("Hello world");
+        }
+    }
+}
